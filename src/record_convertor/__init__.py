23b999931c0418e84986af02c9449bb74cf77728
Maarten de Ruyter <devops@geodatagarden.com>
2024-03-01 20:56:19 +0100

    initial commit

---
+++ src/record_convertor/__init__.py
@@ -0,0 +1,2 @@
+class RecordConvertor():
+    pass
---
+++ tests/test_record_convertor/test_record_convertor.py
@@ -0,0 +1,6 @@
+from record_convertor import RecordConvertor as RC
+
+
+def test_record_convertor_class_exits():
+    """Assert that RecordConvertor class exist."""
+    assert RC()
