79a0223036028447614081d2891b14a95e9d2248
Maarten de Ruyter <devops@geodatagarden.com>
2024-03-01 21:05:33 +0100

    Added initial docstrings

--- src/record_convertor/__init__.py
+++ src/record_convertor/__init__.py
@@ -1,2 +1,24 @@
+"""
+Module to define record conversion functionality.
+
+Classes:
+    - RecordConverter: Public class to be used to convert records based upon
+                       provide rules.
+
+usage:
+>>> converted_record: dict = \
+>>>     RecordConvertor(rules: Rules).convert(record: dict)
+"""
+
+
 class RecordConvertor():
+    """
+    Class that coverts a input record to a format as defined by a yaml file
+    provided when creating the class instance.
+
+
+    public methods:
+        - convert: converts record according to provided rules via the rules class
+
+    """
     pass
