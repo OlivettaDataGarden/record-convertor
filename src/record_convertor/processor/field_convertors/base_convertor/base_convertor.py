bf7c00311d35e352f8e58b11d842ad43c77b479a
Maarten de Ruyter <devops@geodatagarden.com>
2024-03-05 14:51:00 +0100

    isort import order update

--- src/record_convertor/processor/field_convertors/base_convertor/base_convertor.py
+++ src/record_convertor/processor/field_convertors/base_convertor/base_convertor.py
@@ -108,12 +108,11 @@ import jmespath
 import phonenumbers
 from jmespath.exceptions import ParseError
 
-from ....package_settings import EvaluateConditions, BaseRuleDict, BaseConvertorKeys
-from .base_convertor_helpers import (
-    DataFromHTMLSnippet,
-    iso3116_from_alpha_3_country_code,
-    normalize_string,
-)
+from ....package_settings import (BaseConvertorKeys, BaseRuleDict,
+                                  EvaluateConditions)
+from .base_convertor_helpers import (DataFromHTMLSnippet,
+                                     iso3116_from_alpha_3_country_code,
+                                     normalize_string)
 
 __all__ = ["RecordFieldConvertor"]
 
