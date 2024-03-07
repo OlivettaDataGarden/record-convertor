6c19967c14e1092da581cfc40b91739acbce8051
Maarten de Ruyter <devops@geodatagarden.com>
2024-03-07 13:34:19 +0100

    Added protocol class for Field convertor

--- src/record_convertor/package_settings/protocol.py
+++ src/record_convertor/package_settings/protocol.py
@@ -1,7 +1,14 @@
 from typing import Protocol
 
-__all__ = ["ConvertRecordProtocol"]
+from .package_types import BaseRuleDict
+
+__all__ = ["ConvertRecordProtocol", "FieldConvertorProtocol"]
 
 
 class ConvertRecordProtocol(Protocol):
     def convert(self, record: dict) -> dict: ...
+
+
+class FieldConvertorProtocol(Protocol):
+    def __init__(self, record: dict, conversion_rule: BaseRuleDict): ...
+    def convert_field(self) -> dict: ...
