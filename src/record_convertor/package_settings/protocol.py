828aa966be91d359b70e0b64e89a09882e62a397
Maarten de Ruyter <devops@geodatagarden.com>
2024-03-11 22:49:44 +0100

    add public method to recordconvertor protocol and DataClassInstance protocol

--- src/record_convertor/package_settings/protocol.py
+++ src/record_convertor/package_settings/protocol.py
@@ -1,12 +1,23 @@
-from typing import Protocol
+from dataclasses import Field
+from typing import Protocol, ClassVar, Any
 
-from .package_types import BaseRuleDict, FormatDateRuleDict
 
-__all__ = ["RecordConvertorProtocol", "FieldConvertorProtocol", "DateFormatProtocol"]
+from .package_types import BaseRuleDict, FormatDateRuleDict, RulesDict
+
+__all__ = [
+    "RecordConvertorProtocol",
+    "FieldConvertorProtocol",
+    "DateFormatProtocol",
+    "DataclassInstance",
+]
 
 
 class RecordConvertorProtocol(Protocol):
     def convert(self, record: dict) -> dict: ...
+
+    def get_record_convertor_copy_with_new_rules(
+        self, new_rules: RulesDict
+    ) -> "RecordConvertorProtocol": ...
 
 
 class FieldConvertorProtocol(Protocol):
@@ -17,3 +28,7 @@ class DateFormatProtocol(Protocol):
     def format_date_field(
         self, record: dict, conversion_rule: FormatDateRuleDict
     ) -> dict: ...
+
+
+class DataclassInstance(Protocol):
+    __dataclass_fields__: ClassVar[dict[str, Field[Any]]]
