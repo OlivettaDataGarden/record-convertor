f865e41690d792ed61564b3f724e10a11f1ff35c
Maarten de Ruyter <devops@geodatagarden.com>
2024-03-11 22:51:05 +0100

    further extended dataclass processor

--- src/record_convertor/dataclass_processor/__init__.py
+++ src/record_convertor/dataclass_processor/__init__.py
@@ -1,16 +1,32 @@
 from typing import Type
 
-from ..package_settings import class_name_in_snake_case, DataClassRuleDict, DataClassRuleKeys
+from ..package_settings import (
+    class_name_in_snake_case,
+    DataClassRuleDict,
+    DataClassRuleKeys,
+    RecordConvertorProtocol,
+    DataclassInstance
+)
 from dataclasses import is_dataclass
 
-class DataClassProcessor():
-    _data_classes: dict[str, Type] = {}
+
+class DataClassProcessor:
+    def data_from_dataclass(
+        self,
+        record: dict,
+        rules: DataClassRuleDict,
+        record_convertor: RecordConvertorProtocol,
+    ) -> dict:
+        self._record = record
+        self._record_covertor = record_convertor
+        self._prepare_dataclass_settings(rules=rules)
+        return self._create_return_dict(rules=rules) 
+
+        return self._record
 
     def register_dict_of_data_classes(self, dataclasses: dict[str, Type]):
         for dataclass_name, dataclass in dataclasses.items():
-            self._register_dataclass(
-                dataclass_name=dataclass_name,
-                dataclass=dataclass)
+            self._register_dataclass(dataclass_name=dataclass_name, dataclass=dataclass)
 
     def register_data_classes(self, dataclasses: list[Type]):
         for dataclass in dataclasses:
@@ -19,33 +35,62 @@ class DataClassProcessor():
     def register_dataclass(self, dataclass: Type):
         data_class_name_snake_case = class_name_in_snake_case(dataclass.__name__)
         self._register_dataclass(
-            dataclass_name=data_class_name_snake_case,
-            dataclass=dataclass)
+            dataclass_name=data_class_name_snake_case, dataclass=dataclass
+        )
 
     def _register_dataclass(self, dataclass_name: str, dataclass: Type):
         """Method to actually set define dataclass as an attribute of the dataclass processor."""
         if not is_dataclass(dataclass):
             raise ValueError(f"class '{dataclass.__name__}' is not a dataclass")
-        setattr(self, dataclass_name, dataclass)     
-        
+        setattr(self, dataclass_name, dataclass)
 
-    def _prepare_data_class_settings(self, rules: DataClassRuleDict):
-        self._set_dataclass_to_use(rules)    
+    def _prepare_dataclass_settings(self, rules: DataClassRuleDict):
+        self._set_dataclass_to_use(rules)
         self._set_record_covertor_arguments(rules)
         self._set_dataclass_methods(rules)
 
-    def  _set_record_covertor_arguments(self, rules: DataClassRuleDict):
+    def _set_record_covertor_arguments(self, rules: DataClassRuleDict):
         self._record_convertor_args: dict = rules.get(
-            DataClassRuleKeys.RECORD_CONVERSION_ARGUMENTS)
-    
+            DataClassRuleKeys.RECORD_CONVERSION_ARGUMENTS
+        )
+
     def _set_dataclass_methods(self, rules: DataClassRuleDict):
-        self._data_class_methods: list[dict] = rules.get(
-            DataClassRuleKeys.METHODS)
+        self._data_class_methods: list[dict] = rules.get(DataClassRuleKeys.METHODS)
 
     def _set_dataclass_to_use(self, rules: DataClassRuleDict):
         data_class_name = rules.get(DataClassRuleKeys.NAME)
         try:
-            self._data_class_to_use: str = getattr(self, data_class_name)
+            self._data_class_to_use: Type = getattr(self, data_class_name)
         except AttributeError:
             raise ValueError(f"Unknown dataclass '{data_class_name} defines in rules")
-        
+
+    def _get_dataclass_content(self, rules: DataClassRuleDict) -> dict:
+        """Convert input record into dataclass content using provided rules set."""
+        dataclass_content_creator = \
+            self._record_covertor.get_record_convertor_copy_with_new_rules(
+                new_rules=rules)
+        return dataclass_content_creator.convert(record=self._record)
+
+    def _create_return_dict(self, rules: DataClassRuleDict) -> dict:
+        dataclass_content = self._get_dataclass_content(rules)
+        dataclass_instance = self._get_dataclass_instance(dataclass_content)
+    
+    def _get_dataclass_instance(self, dataclass_content: dict) -> DataclassInstance:
+        dataclass_instance = self._data_class_to_use(**dataclass_content)
+        return dataclass_instance
+        dataclass_instance = self._run_methods_on_dataclass(dataclass_instance)
+    
+    def _update_dataclass_with_provided_methods(self, dataclass_instance: DataclassInstance) -> DataclassInstance:
+        for method in self._data_class_methods:
+            [[method, method_argument_rules]] = method.items()
+            method_arguments = self._get_method_arguments(method_argument_rules)
+            for method_argument in method_arguments:
+                getattr(dataclass_instance, method)(**method_argument)
+        return dataclass_instance
+
+    def _get_method_arguments(self, method_argument_rules: dict) -> list[dict]:
+        method_arguments = self._record_covertor.get_record_convertor_copy_with_new_rules(
+                new_rules=method_argument_rules).convert(self._record)
+        return method_arguments if isinstance(method_arguments, list) else [method_arguments]
+            
+    
